c7c783ffe8026f9c866d82de9994e6831286a0b2
Marco Teixeira <marco.bteixeira@gmail.com>
2023-04-25 00:56:31 -0300

    update src to reproduce steps 234

--- src/main.py
+++ src/main.py
@@ -25,6 +25,7 @@ from kivy.uix.textinput import TextInput
 
 from random import randint
 #from kivy.Image import Image
+from matplotlib.colors import Normalize
 
 class MyWidget(Widget):
     pass
@@ -64,13 +65,18 @@ class PyorcApp(App):
 
     def build(self):
         self.index = 0
-    count = 0
+    count = 1
     def run2(self):
-        if self.count == 0:
-            self.step1()
         if self.count == 1:
+            self.step1()
+        if self.count == 2:
             self.step2()
+        if self.count == 3:
+            self.step3()
+        if self.count == 4:
+            self.step4()
         self.count=self.count+1
+
     def step1(self):
         # uncomment line below if you want to view coordinates interactively
         dir=os.getcwd()
@@ -168,5 +174,144 @@ class PyorcApp(App):
         return
         with ProgressBar():
             results = delayed_obj.compute()
+
+    def step3(self):
+        
+        f = plt.figure(figsize=(10, 6))
+        ds = xr.open_dataset("ngwerere_piv.nc")
+        video_file = "ngwerere_20191103.mp4"
+        video = pyorc.Video(video_file, start_frame=0, end_frame=125)
+        video.camera_config = ds.velocimetry.camera_config
+        da_rgb = video.get_frames(method="rgb")
+        da_rgb_proj = da_rgb.frames.project()
+        p = da_rgb_proj[0].frames.plot()
+        ds_mean = ds.mean(dim="time", keep_attrs=True)
+        ds_mean.velocimetry.plot.pcolormesh(
+            ax=p.axes,
+            alpha=0.3,
+            cmap="rainbow",
+            add_colorbar=True,
+            vmax=0.6
+        )
+        ds_mean.velocimetry.plot(
+            ax=p.axes,
+            color="w",
+            alpha=0.5,
+            width=0.0015,
+        )
+
+        ds_filt = ds.velocimetry.filter_temporal()
+        ds_mean_filt = ds_filt.mean(dim="time", keep_attrs=True)
+        p = da_rgb_proj[0].frames.plot()
+        ds_mean_filt.velocimetry.plot(
+            ax=p.axes,
+            alpha=0.4,
+            cmap="rainbow",
+            scale=20,
+            width=0.0015,
+            norm=Normalize(vmax=0.6, clip=False),
+            add_colorbar=True
+        )
+
+        import numpy as np
+        ds_filt2 = ds.velocimetry.filter_temporal(kwargs_angle=dict(angle_tolerance=0.5*np.pi))
+        ds_filt2.velocimetry.filter_spatial(filter_nan=False, inplace=True, kwargs_median=dict(wdw=2))
+        ds_mean_filt2 = ds_filt2.mean(dim="time", keep_attrs=True)
+        p = da_rgb_proj[0].frames.plot()
+
+        ds_mean_filt2.velocimetry.plot(
+            ax=p.axes,
+            alpha=0.4,
+            cmap="rainbow",
+            scale=20,
+            width=0.0015,
+            norm=Normalize(vmax=0.6, clip=False),
+            add_colorbar=True
+        )
+
+        #p = da_rgb_proj[0].frames.plot(mode="geographical")
+        
+        p = da_rgb[0].frames.plot(mode="camera")
+        ds_mean_filt2.velocimetry.plot(
+            ax=p.axes,
+            mode="camera",
+            alpha=0.4,
+            cmap="rainbow",
+            scale=200,
+            width=0.0015,
+            norm=Normalize(vmin=0., vmax=0.6, clip=False),
+            add_colorbar=True
+        )
+        ds_filt2.velocimetry.set_encoding()
+        ds_filt2.to_netcdf("ngwerere_filtered.nc")
+        frame = video.get_frame(0, method="rgb")
+        #da_rgb_proj[0].frames.to_video("3-1.mp4")
+        #print(da_rgb_proj[0].frames)
+        #plt.imshow(frame)
+        p.axes.figure.savefig("3-1.jpg", dpi=200)
+        
+    def step4(self):
+        import pandas as pd
+        ds = xr.open_dataset("ngwerere_filtered_input.nc")
+
+        # also open the original video file
+        video_file = "ngwerere_20191103.mp4"
+        video = pyorc.Video(video_file, start_frame=0, end_frame=1)
+
+        # borrow the camera config from the velocimetry results
+        video.camera_config = ds.velocimetry.camera_config
+
+        # get the frame as rgb
+        da_rgb = video.get_frames(method="rgb")
+
+        cross_section = pd.read_csv("ngwerere_cross_section.csv")
+        x = cross_section["x"]
+        y = cross_section["y"]
+        z = cross_section["z"]
+        cross_section2 = pd.read_csv("ngwerere_cross_section_2.csv")
+        x2 = cross_section2["x"]
+        y2 = cross_section2["y"]
+        z2 = cross_section2["z"]
+        ds_points = ds.velocimetry.get_transect(x, y, z, crs=32735, rolling=4)
+        ds_points2 = ds.velocimetry.get_transect(x2, y2, z2, crs=32735, rolling=4)
+        ds_points_q = ds_points.transect.get_q()
+        ds_points_q2 = ds_points2.transect.get_q()
+        ax = plt.axes()
+        ds_points_q["v_eff"].isel(quantile=2).plot(ax=ax)
+        ds_points_q2["v_eff"].isel(quantile=2).plot(ax=ax)
+        plt.grid()
+
+        norm = Normalize(vmin=0., vmax=0.6, clip=False)
+        p = da_rgb[0].frames.plot(mode="camera")
+        ds.mean(dim="time", keep_attrs=True).velocimetry.plot(
+            ax=p.axes,
+            mode="camera",
+            cmap="rainbow",
+            scale=200,
+            width=0.001,
+            alpha=0.3,
+            norm=norm,
+        )
+        ds_points_q.isel(quantile=2).transect.plot(
+            ax=p.axes,
+            mode="camera",
+            cmap="rainbow",
+            scale=100,
+            width=0.003,
+            norm=norm,
+        )
+        ds_points_q2.isel(quantile=2).transect.plot(
+            ax=p.axes,
+            mode="camera",
+            cmap="rainbow",
+            scale=100,
+            width=0.003,
+            norm=norm,
+            add_colorbar=True
+        )
+        p.axes.figure.savefig("ngwerere.jpg", dpi=200)
+        
+        self.root.add_widget(Picture(source="ngwerere.jpg", center=self.root.center))
+        
 if __name__ == '__main__':
     PyorcApp().run()
